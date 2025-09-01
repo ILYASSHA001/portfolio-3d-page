2e822578a8373eab85aa49fb9cbdc8b21e912f90
Ilyas <ilyas.shalihin@ncs.com.sg>
2025-09-01 13:52:53 +0800

    update build outDir to docs + deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import restart from 'vite-plugin-restart'
 export default {
     root: 'src/',
     publicDir: '../public/',
+    base: '/portfolio2/', 
     plugins:
     [
         // Restart server on static/public file change
