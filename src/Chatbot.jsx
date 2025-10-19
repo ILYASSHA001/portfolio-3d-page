ba03705edd83c21630cfca2b7948da2d43b454e3
Ilyas Shalihin <ilyasshalihin@hotmail.com>
2025-10-19 08:01:40 +0800

    improve chatbot

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -2,11 +2,20 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ChatBot from "@ilyassha001/chatbot";
+import { Button } from 'react-bootstrap'
+import { useState } from 'react';
 
 function AppChat() {
+  const [isChatVisible, setIsChatVisible] = useState(false)
+
+  const chatBotToggler = () => {
+    isChatVisible ? setIsChatVisible(false) : setIsChatVisible(true)
+  }
   return (
     <>
-      <ChatBot />
+      <Button className="chatbot-toggle-button" onClick={chatBotToggler}>toggle Chatbot</Button>
+
+      {isChatVisible && <ChatBot />}
     </>
   )
 }
